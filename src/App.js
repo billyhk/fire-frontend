ab621772175a19bc0e62f5bd70ca6565499a0a04
billyh <billyhkaufman@outlook.com>
2020-05-17 21:58:57 -0400

    top of home page with image and caption

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
 import React from 'react';
 import { Link, Route, Switch } from 'react-router-dom';
 
+//COMPONENTS
+import Home from './components/Home';
+
 const App = () => {
-  return (
-    <>
-    <header>
-      <h1>f i r e</h1>
-    </header>
-    <main>
-        <h2>detailed content...</h2>
-    </main>
-    </>
-  )
-}
+	return (
+		<>
+			<header className='app-header'>
+				<h1>f i r e</h1>
+				<div className='nav-button'>
+					<Link to='/'>home</Link>
+					<Link to='strains/create'>add strain</Link>
+					<Link to='/strains'>all strains</Link>
+				</div>
+			</header>
+			<main>
+				<Switch>
+					<Route exact path='/' component={Home} />
+					{/* <Route exact path='/strains/create' component={Create} />
+					<Route exact path='/strains' component={StrainsList} />
+					<Route exact path='/strains/:id' component={Show} />
+					<Route exact path='/strains/:id/edit' component={Edit} /> */}
+				</Switch>
+			</main>
+		</>
+	);
+};
 
 export default App;
---
+++ src/components/Home.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Home = () => (
+	<>
+		<header className='home-header'>
+			{/* <img
+				src=''
+				alt='widow widow plant head'
+				className='header-image'
+			/> */}
+			<div className='home-header-text'>
+				<h1>
+					welcome to <span className='home-header-logo'>f i r e</span>
+				</h1>
+				<h2>{'explore \u00b7 edit \u00b7 create'}</h2>
+			</div>
+		</header>
+	</>
+);
+
+export default Home;
