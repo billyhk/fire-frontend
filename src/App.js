f5f1935ae0a94b3897ed1804a17ed304b97b0b85
andresaugusto-ga <andres.augusto.ga@icloud.com>
2020-05-22 12:24:18 -0400

    completed initial style scheme for deployment

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ const App = () => {
 
 	return (
 		<>
-			<Modal isShowing={isShowing} hide={toggle} />
+			{/* <Modal isShowing={isShowing} hide={toggle} /> */}
 			<header>
 				<nav>
 					<Link className='links' to='/'>
