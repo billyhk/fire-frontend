7d7189b108542c9ceb4d9510a6415584e0876812
billyh <billyhkaufman@outlook.com>
2020-06-21 13:26:08 -0400

    change deploy method from gh-pages to heroku

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import './index.css';
 import App from './App';
 
 ReactDOM.render(
-	<Router basename='/FIRE-front'>
+	<Router basename='/'>
 		<App />
 	</Router>,
 	document.getElementById('root')
